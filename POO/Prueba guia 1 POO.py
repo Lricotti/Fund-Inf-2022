a1ec0def3193cc0e358c1766278dbd71a7180eaf
Lricotti <luchoricotti@gmail.com>
2022-04-28 15:50:46 -0300

    A

--- POO/Prueba guia 1 POO.py
+++ POO/Prueba guia 1 POO.py
@@ -101,7 +101,7 @@ contador.valorActual()
 
 #Definí una clase que modele un contador, el cual puede incrementar o disminuir en uno el valor que se ingresa, recordando el valor actual.
 #También puede resetear este valor y al hacerlo se pone en cero. Además es posible indicar directamente un número nuevo que reemplace al valor actual. 
-#Este objeto debe entender los siguientes mensajes:
+# Este objeto debe entender los siguientes mensajes  :
 
 class Contador_2():
     def __init__(self, Numero_inicial):
